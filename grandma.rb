74e3cbd625acc3ca2306071610f7e966a7b94ac9
Patrick Jones <pmichaeljones@gmail.com>
2014-03-31 10:48:20 -0600

    Finished project. Could use some DRYing up though.

--- grandma.rb
+++ grandma.rb
@@ -7,14 +7,21 @@ while true
 
 	when input_text.downcase #if text is downcase
 		puts "HUH? Speak up Sonny!"
+		print ">> "
 		input_text = gets.chomp()
 
-	when input_text.upcase #if text is yelling (all caps)
-		puts "No, not since 1938."
+	when input_text.capitalize #if it's capcase
+		puts "Huh? Speak up Sonny!"
+		print ">> "
 		input_text = gets.chomp()
 
 	when "BYE!" #exit process
 		puts "You have a great day!"
 		Process.exit(0)
+
+	when input_text.upcase #if text is yelling (all caps)
+		puts "No, not since 1938."
+		print ">> "
+		input_text = gets.chomp()
 	end
 end
