ff5aac0cd7fdd7e414765a73fdbe0ffedf255bb5
Patrick Jones <pmichaeljones@gmail.com>
2014-03-31 08:30:22 -0600

    Finished BeerSong

---
+++ beersong.rb
@@ -0,0 +1,9 @@
+initial_beer = 99
+
+while initial_beer >= 1
+	puts "#{initial_beer} bottles of beers on the wall"
+	puts "#{initial_beer} bottles of beer."
+	initial_beer -= 1
+	puts "Take one down, pass it around, #{initial_beer} bottles of beer on the wall."
+end
+
