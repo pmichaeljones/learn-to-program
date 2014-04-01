0fdde79e2fdb3e2602a5b4ed4329f055b8cc1c53
Patrick Jones <pmichaeljones@gmail.com>
2014-04-01 08:37:46 -0600

    Finished old school roman numberals

--- old_school_roman_numerals.rb
+++ old_school_roman_numerals.rb
@@ -5,13 +5,48 @@ def old_school_roman_numeral(year)
 		puts
 	else
 		(year/1000).times { print "M" }
-		puts "Reminder: #{year % 1000}"
+		remainder = year % 1000
+			if remainder % 500 == 0
+				(remainder/500).times { print "D" }
+				puts
+			else
+				(remainder/500).times {print "D" }
+				remainder = remainder % 500
+					if remainder % 100 == 0
+						(remainder/100).times { print "C" }
+						puts
+					else
+						(remainder/100).times {print "C" }
+						remainder = remainder % 100
+							if remainder % 50 == 0
+								(remainder/50).times {print "L" }
+								puts
+							else
+								(remainder/50).times {print "L" }
+								remainder = remainder % 50
+									if remainder % 10 == 0
+										(remainder/10).times {print "X" }
+										puts
+									else
+										(remainder/10).times {print "X" }
+										remainder = remainder % 10
+											if remainder % 5 == 0
+												(remainder/5).times {print "V" }
+												puts
+											else
+												(remainder/5).times {print "V" }
+												remainder = remainder % 5
+												(remainder/1).times {print "I" }
+												puts
+											end
+									end
+							end
+					end
+
+			end
 	end
-
-
 
 end
 
-old_school_roman_numeral(1000)
-old_school_roman_numeral(5000)
-old_school_roman_numeral(7000)
+
+old_school_roman_numeral(6573)
