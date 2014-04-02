d989d036001d98d183c85cfa1fd09656da158a3b
Patrick Jones <pmichaeljones@gmail.com>
2014-04-02 14:53:42 -0600

    Scrapping V1, moving onto V2

--- english_number.rb
+++ english_number.rb
@@ -9,12 +9,41 @@ def say_it_in_english_please(number)
 
 	number_array = number.to_s.split("") #split the number into separate digits
 
+	array_length = number_array.length 
+
 	number_array.each do |x|
 	
-		if x == 1
-			puts "One-"
+		if array_length == 1
+			if x == "1"
+				puts "One"
+			elsif x == "2"
+				puts "Two"
+			elsif x == "3"
+				puts "Three"
+			elsif x == "4"
+				puts "Four"
+			elsif x == "5"
+				puts "Five"
+			elsif x == "6"
+				puts "Six"
+			elsif x == "7" 
+				puts "Seven"
+			elsif x == "8"
+				puts "Eight"
+			elsif x == "9"
+				puts "Nine"
+			end #finished 1-digit length loop
 
-	end
+		elsif array_length == 2
+			if x == "1" && x+1 == "2"
+				puts "Twelve"
+			end
+
+		else #meaning 3-digit number
+			puts "One-Hundred"
+		end #finishes the array_length if statement
+
+	end #finished the do statement
 
 
 end
---
+++ english_number_2.rb
@@ -0,0 +1,16 @@
+def second_number(number)
+	number_as_array = number.to_s.split("")
+
+	if number_as_array[1] == "1"
+		return puts "One"
+	end
+
+end
+
+
+
+def english_number(number)
+
+
+
+end
