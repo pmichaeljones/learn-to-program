f6fef5d1cae15bddac3121b45a9fecce9832398f
Patrick Jones <pmichaeljones@gmail.com>
2014-04-03 10:52:40 -0600

    Initial Commit

---
+++ 99_bottles.rb
@@ -0,0 +1,11 @@
+def sing_beer_song(number_of_bottles)
+	#set some key variables
+	small_numbers = ['one', 'two', 'three', 'four', 'five', 'six',
+		'seven', 'eight', 'nine', 'ten']
+	teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+		'sixteen', 'seventeen', 'eighteen', 'nineteen']
+	middle_numbers = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+		'eighty', 'ninety']
+	hundred = "hundred"
+	thousand = "thousand"
+	
