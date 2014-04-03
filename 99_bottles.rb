cd2e72437bceb2a65200e409b0a2b2a870676513
Patrick Jones <pmichaeljones@gmail.com>
2014-04-03 11:45:35 -0600

    recursion worked

--- 99_bottles.rb
+++ 99_bottles.rb
@@ -26,52 +26,53 @@ def sing_beer_song(number_of_bottles)
 		'8' => 'eighty',
 		'9' => 'ninety'
 	}
+
 	hundred = "hundred"
 	thousand = "thousand"
 
 	#create english number answer string
+
 	english_number = ''
 
 	#break up number into string to see how many digits it contains
 
-	bottles_as_array = number_of_bottles.to_s.split("")
+	bottle_as_array = number_of_bottles.to_s.split("")
 
-	while bottle_as_array.length > 0
+		while bottle_as_array.length > 0 do
 
-		if bottle_as_array.length == 4 #we need to work out thousands
-			puts "#{small_numbers[bottle_as_array[0]]}" + thousand
-			#remove one digit
-			bottle_as_array = bottle_as_array.shift()
-			#make it an integer again
-			bottle_as_array_to_integer = bottle_as_array.join("").to_i
-			#recursion time!
-			sing_beer_song(bottle_as_array_to_integer)
+			if bottle_as_array.length == 4 #we need to work out thousands
+				puts english_number + "#{small_numbers[bottle_as_array[0]]}-" + thousand
+				#remove one digit
+				bottle_as_array = bottle_as_array.pop(3)
+				#make it an integer again
+				bottle_as_array_to_integer = bottle_as_array.join("").to_i
+				#recursion time!
+				sing_beer_song(bottle_as_array_to_integer)
 
-		if bottle_as_array.length == 3 #we have to work out hundreds
-			puts "#{small_numbers[bottle_as_array[0]]}" + hundred
-			#remove one digit
-			bottle_as_array = bottle_as_array.shift()
-			#make it an integer again
-			bottle_as_array_to_integer = bottle_as_array.join("").to_i
-			#recursion time!
-			sing_beer_song(bottle_as_array_to_integer)
+			elsif bottle_as_array.length == 3 #we have to work out hundreds
+				puts english_number+ "#{small_numbers[bottle_as_array[0]]}-" + hundred
+				#remove one digit
+				bottle_as_array = bottle_as_array.pop(2)
+				#make it an integer again
+				bottle_as_array_to_integer = bottle_as_array.join("").to_i
+				#recursion time!
+				sing_beer_song(bottle_as_array_to_integer)
 
-		if bottle_as_array.length == 2 #we are working with # < 100 now
-			puts "#{small_numbers[bottle_as_array[0]]}"
-			#remove one digit
-			bottle_as_array = bottle_as_array.shift()
-			#make it an integer again
-			bottle_as_array_to_integer = bottle_as_array.join("").to_i
-			#recursion time!
-			sing_beer_song(bottle_as_array_to_integer)
-		
-		if bottle_as_array.legnth == 1 #final digit time
-		
-		end
+			elsif bottle_as_array.length == 2 #we are working with # < 100 now
+				puts english_number+ "#{middle_numbers[bottle_as_array[0]]}-"
+				#remove one digit
+				bottle_as_array = bottle_as_array.pop()
+				#make it an integer again
+				bottle_as_array_to_integer = bottle_as_array.join("").to_i
+				#recursion time!
+				sing_beer_song(bottle_as_array_to_integer)
+			
+			elsif bottle_as_array.length == 1 #final digit time
+			
+			end #closes the IF statements
 
-	end
+		end #closes the while loop
 
 	puts english_number
 
-	
-
+end
