fc081c6a4da93ae1dac9df0d375c81576b526913
Patrick Jones <pmichaeljones@gmail.com>
2014-04-05 11:55:50 -0600

    Created english version of Ruby steps to solve problem

---
+++ music_shuffle.rb
@@ -0,0 +1,11 @@
+# Steps to Create a Playlist Maker
+
+# 1. Name Playlist with Chomp
+# 2. Create File Name
+# 3. Find Music Using Regex
+# 4. Ask how many of the directories to add
+# 5. Ask how many files
+# 6. Put files into array
+# 7. Shuffle that bitch using my method from before. Use INCLUDE and module for practice
+# 8. Output the shuffled array into new files
+# 9. Play that bitch on music player to test it
